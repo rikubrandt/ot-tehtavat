d71fda653be6a38c3bc5210daede4964489d3829
Riku Brandt <brandtriku@gmail.com>
2022-03-23 16:40:22 +0200

    Teht 6

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,24 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_kortin_saldo_alussa(self):
+        self.assertEqual(self.maksukortti.saldo, 10)
+
+    def test_rahan_lataaminen(self):
+        self.maksukortti.lataa_rahaa(2)
+        self.assertEqual(self.maksukortti.saldo, 12)
+
+    def test_rahan_ottaminen_nakyy_saldossa(self):
+        self.maksukortti.ota_rahaa(2)
+        self.assertEqual(self.maksukortti.saldo, 8)
+
+    def test_liian_paljon_rahan_ottamista_ei_vahenna(self):
+        self.maksukortti.ota_rahaa(12)
+        self.assertEqual(self.maksukortti.saldo, 10)
+
+    def test_rahan_ottaminen_palauttaa_arvon(self):
+        arvo = self.maksukortti.ota_rahaa(12)
+        self.assertFalse(arvo)
+        arvo = self.maksukortti.ota_rahaa(5)
+        self.assertTrue(arvo)
