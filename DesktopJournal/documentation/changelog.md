38ab8d953770b2ef2265a1b9e2cafcbeed95f4ad
Riku Brandt <brandtriku@gmail.com>
2022-03-31 14:35:13 +0300

    Repositories

---
+++ DesktopJournal/documentation/changelog.md
@@ -0,0 +1,4 @@
+# Changelog
+
+### Week 3
+- Created databases
---
+++ DesktopJournal/src/repositories/notes_repository.py
@@ -0,0 +1,15 @@
+
+
+
+
+class NotesRepository:
+    
+    def __init__(self, connection):
+        self.connection = connection
+
+    def get_all_notes(self):
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT id, text, time FROM notes WHERE visible = 1")
+        rows = cursor.fetchall()
+        print(rows)
+        return rows
